380b5d0e8e29d99952e1b96a9e47fcb19829ce77
Sara <sespinogallardo@safareyes.es>
2025-04-23 14:26:46 +0200

    dia subido

--- DOGPUCCINObasedatos.sql
+++ DOGPUCCINObasedatos.sql
@@ -1,3 +1,4 @@
+
 CREATE TABLE Clientes (
     ID number primary key,
     Nombre varchar2(20),
@@ -9,28 +10,14 @@ CREATE TABLE Clientes (
     Ciudad varchar2(50),
     Correo_Electronico varchar2(50)
 );
+
 CREATE TABLE Usuarios (
     ID number primary key,
     Contraseña varchar2(15) not null,
-    Rol varchar2(10) not null
+    Rol varchar2(10) not null,
+    foreign KEY (ID) references Clientes(ID) 
 );
-CREATE TABLE Perros (
-    ID number primary key,
-    Nombre varchar2(20),
-    Fecha_Nacimiento date,
-    Sexo char(1) check (Sexo in ('M', 'H')),
-    Calle varchar2(50),
-    Ciudad varchar2(50),
-    Adoptado char(1)Check (Adoptado in ('S', 'N'))
-);
-CREATE TABLE Notificaciones (
-    ID number primary key,
-    Tipo varchar2(50),
-    Mensaje varchar2(500),
-    Fecha_Envio date,
-    Usuario_ID number,
-    foreign KEY (Usuario_ID) references Usuarios(ID)
-);
+
 CREATE TABLE Protectoras (
     CIF varchar2(20) primary key,
     Nombre varchar2(100),
@@ -38,19 +25,51 @@ CREATE TABLE Protectoras (
     Correo_Electronico varchar2(50),
     Calle varchar2(50),
     Ciudad varchar2(50),
-    Redes_Sociales varchar2(100)
+    Redes_Sociales varchar2(100),
+    ID_Usuario number,  -- Agregamos la columna ID_Usuario
+    foreign KEY (ID_Usuario) references Usuarios(ID)
 );
+
+
+
+CREATE TABLE Perros (
+    ID number primary key,
+    Nombre varchar2(20),
+    Fecha_Nacimiento date,
+    Sexo char(1) check (Sexo in ('M', 'H')),
+    Calle varchar2(50),
+    Ciudad varchar2(50),
+    Adoptado char(1) check (Adoptado in ('S', 'N')),
+    CIF varchar2(20), 
+    FOREIGN KEY (CIF) REFERENCES Protectoras(CIF) 
+);
+
+
+CREATE TABLE Notificaciones (
+    ID number primary key,
+    Tipo varchar2(50),
+    Mensaje varchar2(500),
+    Fecha_Envio date,
+    Usuario_ID number,
+    FOREIGN KEY (Usuario_ID) REFERENCES Usuarios(ID)
+);
+
+
 CREATE TABLE Patologias (
-    NOMBRE varchar2 primary key
+     ID number primary key, 
+    NOMBRE varchar2(50)
 );
+
+
 CREATE TABLE Perros_Patologias (
     ID_Perros number,
     ID_Patologia number,
     Descripcion varchar(100),
     primary KEY (ID_Perros, ID_Patologia),
-    foreign KEY (ID_Perros) references Perros(ID),
-    foreign KEY (ID_Patologia) references Patologias(ID)
+    FOREIGN KEY (ID_Perros) REFERENCES Perros(ID),
+    FOREIGN KEY (ID_Patologia) REFERENCES Patologias(ID)
 );
+
 CREATE TABLE Reservan (
     Cliente_ID number,
     Perro_ID number,
@@ -62,6 +81,8 @@ CREATE TABLE Reservan (
     foreign key (Cliente_ID) references Clientes(ID),
     foreign key (Perro_ID) references Perros(ID)
 );
+
+
 CREATE TABLE Solicitud_adopcion (
     Cliente_ID number,
     Perro_ID number,
@@ -69,9 +90,27 @@ CREATE TABLE Solicitud_adopcion (
     foreign key (Cliente_ID) references Clientes(ID),
     foreign key (Perro_ID) references Perros(ID)
 );
+
+
 CREATE TABLE Razas (
-    Tipo varchar2 primary key
+    Tipo varchar2(50) primary key,
+    ID_Perros number,
+    FOREIGN KEY (ID_Perros) REFERENCES Perros(ID)  
 );
+
+/*
+    DROP TABLE CLIENTES;
+    DROP TABLE NOTIFICACIONES;
+    DROP TABLE PERROS;
+    DROP TABLE PROTECTORAS;
+    DROP TABLE RESERVAN;
+    DROP TABLE SOLICITUD_ADOPCION;
+    DROP TABLE USUARIOS;
+    DROP TABLE patologias;
+    drop table perros_patologias;
+    drop table razas;
+*/
+
 
 
 
