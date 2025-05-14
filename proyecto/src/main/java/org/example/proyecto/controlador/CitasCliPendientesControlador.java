7a95059da3061eb58dd6e9c1f3c03341fb21b464
rubenromero17 <ruromeroaponte@safareyes.es>
2025-05-14 23:44:46 +0200

    vistas136

---
+++ proyecto/src/main/java/org/example/proyecto/controlador/CitasCliPendientesControlador.java
@@ -0,0 +1,134 @@
+package org.example.proyecto.controlador;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class CitasCliPendientesControlador {
+    @FXML
+    private Button btnAtras;
+    @FXML
+    private Button btnPerros;
+    @FXML
+    private Button btnNotificaciones;
+    @FXML
+    private Button btnCitas;
+    @FXML
+    private Button btnSobreNosotros;
+    @FXML
+    private Button btnCitasPasadas;
+    @FXML
+    private Button btnCitasPendientes;
+
+    @FXML
+    private void btnAtras(ActionEvent event) {
+        try {
+            FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/org/example/proyecto/VistaInicio.fxml"));
+            Parent root = fxmlLoader.load();
+
+            Stage stage = (Stage) btnAtras.getScene().getWindow();
+
+            Scene scene = new Scene(root);
+            stage.setScene(scene);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+    @FXML
+    private void btnPerros(ActionEvent event) {
+        try {
+            FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/org/example/proyecto/VistaPerrosCli.fxml"));
+            Parent root = fxmlLoader.load();
+
+            Stage stage = (Stage) btnPerros.getScene().getWindow();
+
+            Scene scene = new Scene(root);
+            stage.setScene(scene);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+    @FXML
+    private void btnNotificaciones(ActionEvent event) {
+        try {
+            FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/org/example/proyecto/VistaNotCliCancelacion.fxml"));
+            Parent root = fxmlLoader.load();
+
+            Stage stage = (Stage) btnNotificaciones.getScene().getWindow();
+
+            Scene scene = new Scene(root);
+            stage.setScene(scene);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+    @FXML
+    private void btnCitas(ActionEvent event) {
+        try {
+            FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/org/example/proyecto/VistaCitasCliPendientes.fxml"));
+            Parent root = fxmlLoader.load();
+
+            Stage stage = (Stage) btnCitas.getScene().getWindow();
+
+            Scene scene = new Scene(root);
+            stage.setScene(scene);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+    @FXML
+    private void btnSobreNosotros(ActionEvent event) {
+        try {
+            FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/org/example/proyecto/VistaSobreNosotrosCli.fxml"));
+            Parent root = fxmlLoader.load();
+
+            Stage stage = (Stage) btnSobreNosotros.getScene().getWindow();
+
+            Scene scene = new Scene(root);
+            stage.setScene(scene);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+    @FXML
+    private void btnCitasPasadas(ActionEvent event) {
+        try {
+            FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/org/example/proyecto/VistaCitasCliPasadas.fxml"));
+            Parent root = fxmlLoader.load();
+
+            Stage stage = (Stage) btnCitasPasadas.getScene().getWindow();
+
+            Scene scene = new Scene(root);
+            stage.setScene(scene);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+    @FXML
+    private void btnCitasPendientes(ActionEvent event) {
+        try {
+            FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/org/example/proyecto/VistaCitasCliPendientes.fxml"));
+            Parent root = fxmlLoader.load();
+
+            Stage stage = (Stage) btnCitasPendientes.getScene().getWindow();
+
+            Scene scene = new Scene(root);
+            stage.setScene(scene);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+}
